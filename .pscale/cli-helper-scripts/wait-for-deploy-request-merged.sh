66dca6394a740f81d048720d9b1c0511199646ba
Brian Morrison II <brian@brianmorrison.me>
2023-03-16 15:35:20 -0400

    check dr for in_progress_cutover

--- .pscale/cli-helper-scripts/wait-for-deploy-request-merged.sh
+++ .pscale/cli-helper-scripts/wait-for-deploy-request-merged.sh
@@ -24,7 +24,7 @@ function wait_for_deploy_request_merged {
         fi
         local output=`echo $raw_output | jq ".[] | select(.number == $number) | .deployment.state"`
         # test whether output is pending, if so, increase wait timeout exponentially
-        if [ "$output" = "\"pending\"" ] || [ "$output" = "\"in_progress\"" ] || [ "$output" = "\"submitting\"" ]; then
+        if [ "$output" = "\"pending\"" ] || [ "$output" = "\"in_progress\"" ] || [ "$output" = "\"submitting\"" ] || [ "$output" = "\"in_progress_cutover\"" ]; then
             # increase wait variable exponentially but only if it is less than max_timeout
             if [ $((wait * 2)) -le $max_timeout ]; then
                 wait=$((wait * 2))
